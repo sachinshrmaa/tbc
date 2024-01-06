01b1f558c9835b6039afb7569af3e9d2f935e0a1
sachinshrmaa <krisa1826@gmail.com>
2024-01-06 08:58:15 +0530

    feat: add about page

---
+++ app/(frontend)/about/page.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <main className="max-w-7xl mx-auto px-4 py-6">
+      <h1 className="text-lg md:text-5xl mb-4 font-normal md:font-medium">
+        About
+      </h1>
+      <p className="my-3">
+        The Basics Class or TBC is an initiative by Sachin Sharma to personally
+        teach students in the field of Software Engineering.
+      </p>
+      <p className="my-3">
+        Sachin strongly believes that programming skills should be taught in a
+        live manner and by building real-world projects rather than just
+        understanding the syntax and the concepts.
+      </p>
+      <p className="mb-5">
+        All the courses and workshops are a series of live cohorts, focusing
+        more on building projects and Open source contributions and you get to
+        interact with the instructor whenever you get stuck.
+      </p>
+
+      <h1 className="text-lg md:text-5xl my-5 font-normal md:font-medium">
+        Sachin Sharma
+      </h1>
+      <p className="my-3">
+        Sachin is currently working as an Intern for a Cloud-based startup
+        company, where he focuses on works related to frontend development.
+      </p>
+      <p className="my-3">
+        He’&apos;s also pursuing his Bachelor's degree in Computer Engineering
+        from SIST (Sikkim Institute of Science and Technology).
+      </p>
+      <p className="my-3">
+        Previously, he also completed his Diploma in Computer Engineering with a
+        distinction grade. Apart from studies and work, he actively builds and
+        contributes to open-source projects on GitHub.
+      </p>
+    </main>
+  );
+}
