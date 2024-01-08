026a40d2c1466e3725e2bd3cdce9c2929089f965
sachinshrmaa <krisa1826@gmail.com>
2024-01-08 22:38:46 +0530

    feat: add contact page

---
+++ app/(frontend)/contact/page.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <main className="max-w-7xl mx-auto px-4 py-6">
+      <h1 className="text-lg md:text-5xl mb-4 font-normal md:font-medium">
+        Contact
+      </h1>
+      <p className="my-3">
+        Thank you for visiting the basics website! If you have any questions or
+        comments, we would love to hear from you.
+      </p>
+      <p>
+        <b>Mail:</b> mail.sachinshrmaa@gmail.com
+      </p>
+    </main>
+  );
+}
