5566241a6270308262b4a6ed5c1a9ef888a327bf
sachinshrmaa <krisa1826@gmail.com>
2024-01-11 20:42:10 +0530

    style: about and contact page

--- app/(frontend)/about/page.jsx
+++ app/(frontend)/about/page.jsx
@@ -1,42 +1,87 @@
+// import React from "react";
+
+// export default function About() {
+//   return (
+//     <main className="max-w-7xl mx-auto px-4 py-6">
+//       <h1 className="text-lg md:text-5xl mb-4 font-normal md:font-medium">
+//         About
+//       </h1>
+
+
+//       <h1 className="text-lg md:text-5xl my-5 font-normal md:font-medium">
+//         Sachin Sharma
+//       </h1>
+//       <p className="my-3">
+//         Sachin is currently working as an Intern for a Cloud-based startup
+//         company, where he focuses on works related to frontend development.
+//       </p>
+//       <p className="my-3">
+//         He&apos;s also pursuing his Bachelor&apos;s degree in Computer Engineering
+//         from SIST (Sikkim Institute of Science and Technology).
+//       </p>
+//       <p className="my-3">
+//         Previously, he also completed his Diploma in Computer Engineering with a
+//         distinction grade. Apart from studies and work, he actively builds and
+//         contributes to open-source projects on GitHub.
+//       </p>
+//     </main>
+//   );
+// }
+
 import React from "react";
 
 export default function About() {
   return (
-    <main className="max-w-7xl mx-auto px-4 py-6">
-      <h1 className="text-lg md:text-5xl mb-4 font-normal md:font-medium">
+    <div className="max-w-7xl mx-auto px-5 py-6 min-h-[70vh]">
+      <h1 className="text-3xl md:text-5xl mb-5 md:mb-10 font-normal md:font-bold text-center">
         About
       </h1>
-      <p className="my-3">
-        The Basics Class or TBC is an initiative by Sachin Sharma to personally
-        teach students in the field of Software Engineering.
-      </p>
-      <p className="my-3">
-        Sachin strongly believes that programming skills should be taught in a
-        live manner and by building real-world projects rather than just
-        understanding the syntax and the concepts.
-      </p>
-      <p className="mb-5">
-        All the courses and workshops are a series of live cohorts, focusing
-        more on building projects and Open source contributions and you get to
-        interact with the instructor whenever you get stuck.
-      </p>
 
-      <h1 className="text-lg md:text-5xl my-5 font-normal md:font-medium">
-        Sachin Sharma
-      </h1>
-      <p className="my-3">
-        Sachin is currently working as an Intern for a Cloud-based startup
-        company, where he focuses on works related to frontend development.
-      </p>
-      <p className="my-3">
-        He&apos;s also pursuing his Bachelor&apos;s degree in Computer Engineering
-        from SIST (Sikkim Institute of Science and Technology).
-      </p>
-      <p className="my-3">
-        Previously, he also completed his Diploma in Computer Engineering with a
-        distinction grade. Apart from studies and work, he actively builds and
-        contributes to open-source projects on GitHub.
-      </p>
-    </main>
+
+      <section className="rounded-3xl md:mb-12">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="rounded-2xl p-6 md:p-12 bg-slate-100">
+            <h1 className="text-xl md:text-3xl md:mb-5 font-semibold mb-2">
+              TBC
+            </h1>
+            <p className="my-3">
+              The Basics Class or TBC is an initiative by Sachin Sharma to
+              personally teach students in the field of Software Engineering.
+            </p>
+            <p className="my-3">
+              Sachin strongly believes that programming skills should be taught
+              in a live manner and by building real-world projects rather than
+              just understanding the syntax and the concepts.
+            </p>
+            <p className="mb-5">
+              All the courses and workshops are a series of live cohorts,
+              focusing more on building projects and Open source contributions
+              and you get to interact with the instructor whenever you get
+              stuck.
+            </p>
+          </div>
+          <div className="rounded-2xl p-6 md:p-12 bg-slate-100">
+            <h1 className="text-xl md:text-3xl md:mb-5 font-semibold mb-2">
+              About Sachin
+            </h1>
+            <p className="my-3">
+              Sachin is currently working as an Intern for a Cloud-based startup
+              company, where he focuses on works related to frontend
+              development.
+            </p>
+            <p className="my-3">
+              He&apos;s also pursuing his Bachelor&apos;s degree in Computer
+              Engineering from SIST (Sikkim Institute of Science and
+              Technology).
+            </p>
+            <p className="my-3">
+              Previously, he also completed his Diploma in Computer Engineering
+              with a distinction grade. Apart from studies and work, he actively
+              builds and contributes to open-source projects on GitHub.
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
--- app/(frontend)/contact/page.jsx
+++ app/(frontend)/contact/page.jsx
@@ -2,17 +2,36 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <main className="max-w-7xl mx-auto px-4 py-6">
-      <h1 className="text-lg md:text-5xl mb-4 font-normal md:font-medium">
-        Contact
+    <div className="max-w-7xl mx-auto px-5 py-6 min-h-[70vh]">
+      <h1 className="text-3xl md:text-5xl mb-6 md:mb-10 font-normal md:font-bold text-center">
+        Contact Us
       </h1>
-      <p className="my-3">
-        Thank you for visiting the basics website! If you have any questions or
-        comments, we would love to hear from you.
-      </p>
-      <p>
-        <b>Mail:</b> mail.sachinshrmaa@gmail.com
-      </p>
-    </main>
+
+      <div className="grid grid-cols-1 md:grid-cols-4 md:gap-6">
+        <div className="col-span-1 rounded-3xl grid gap-6 mb-6 md:mb-0">
+          <div className="bg-slate-100 rounded-3xl p-6">
+            <p>
+              <b>Email: </b>mail.sachinshrmaa@gmail.com
+            </p>
+          </div>
+          <div className="bg-slate-100 rounded-3xl p-6">
+            <p>
+              <b>Phone: </b>+91 9382341382
+            </p>
+          </div>
+        </div>
+        <div className="col-span-3 rounded-3xl bg-slate-100 p-10">
+          <p className="my-4">
+            Thank you for visiting the basics website! If you have any questions
+            or comments, we would love to hear from you.
+          </p>
+
+          <p className="my-4">
+            We strive to respond to all inquiries within one business day. Thank
+            you for choosing us. We look forward to serving you!
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
