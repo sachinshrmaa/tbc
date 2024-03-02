f47b5457ab7529e20b61b1d28cb26201893ea297
sachinshrmaa <krisa1826@gmail.com>
2024-03-02 20:22:37 +0530

    fix: apostrophy symbol

--- src/app/components/InstructorCard.tsx
+++ src/app/components/InstructorCard.tsx
@@ -6,7 +6,13 @@ export default function InstructorCard() {
   return (
     <div className="secondary-bg rounded-lg p-5 grid md:grid-flow-col grid-cols-8 gap-2 md:gap-5">
       <div className="col-span-8 md:col-span-2">
-        <Image src="/sachin.jpg" alt="sachin sharma" height="500" width="400" className="border-2 border-purple-600 rounded-lg " />
+        <Image
+          src="/sachin.jpg"
+          alt="sachin sharma"
+          height="500"
+          width="400"
+          className="border-2 border-purple-600 rounded-lg "
+        />
       </div>
       <div className="col-span-8 md:col-span-6">
         <p className="text-slate-400 my-2 text-sm md:text-md">
@@ -18,10 +24,10 @@ export default function InstructorCard() {
           work done and figure out the rest by yourself?
         </p>
         <p className="text-slate-400 my-2 text-sm md:text-md">
-          Hi, I'm Sachin👋, currently working as an Intern for a cloud-based
-          startup company, where I work on things related to frontend
-          development. I'm also pursuing my Bachelor's degree in Computer
-          Engineering.
+          Hi, I&apos;m Sachin👋, currently working as an Intern for a
+          cloud-based startup company, where I work on things related to
+          frontend development. I&apos;m also pursuing my Bachelor&apos;s degree
+          in Computer Engineering.
         </p>
       </div>
     </div>
